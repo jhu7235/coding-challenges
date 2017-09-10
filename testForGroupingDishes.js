658302a05b7cdbdc87f9c5add0b65970faa76df3
Jason Hu <jhu7235@github.com>
2017-09-10 14:13:26 -0500

    new problem solved

---
+++ testForGroupingDishes.js
@@ -0,0 +1,34 @@
+// ingredient generator
+
+let ingredientClass = ['red', 'blue', 'green', 'yellow', 'purple'];
+let ingredientTypes = [];
+let ingredientClassCount = 0;
+while(ingredientClassCount < ingredientClass.length) {
+	let ingGenCount = 0;
+	while(ingGenCount < 50) {
+		ingredientTypes.push(ingredientClass + ingGenCount++);
+	}
+	ingredientClassCount++;
+}
+
+
+// add dish
+let dishCount = 0;
+let dishes = [];
+while(dishCount < 500) {
+	let ingredients = [];
+	while(ingredientCount < 10) {
+		ingredients.push();
+		ingredientCount++
+	}
+	dishes.puhs(ingredients);
+	dishCount++;
+}
+count = 0;
+
+
+// add ingredient
+
+while(count<50) {
+	
+}
