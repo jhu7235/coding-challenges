54dec8c2da8783a5911784cd375386515b80f796
Jason Hu <jhu7235@github.com>
2017-10-09 11:23:16 -0500

    class for polynomial calculation

---
+++ Polynomial.js
@@ -0,0 +1,39 @@
+
+
+// Question #1:
+/* You're an esteemed engineer at Math & Co. in their Algebraic Infrastructure team. The company is pivoting towards using this new technology that is based on polynomials and many of your peers are on the verge of creating servers that will crunch polynomials. 
+
+You have been tasked with designing a class to optimally represent a polynomial for their use. This class would need to be high-performance and cater to the operations that can be performed on polynomials. Please provide commentary on your thought process in your design and the code for this class in a language of your choice. */
+
+// Polynomials should take in a string.  Each element in the array should represent the coeefficents of a unique variable. For example [[2,1,4,2]]
+export class Polynomial {
+	constructor(poly) {
+		// code
+		poly.split()
+	}
+
+	// methods
+	add() {
+
+	}
+	subtract() {
+
+	}
+
+	multiply() {
+
+	}
+
+	syntheticDivide() {
+
+	}
+}
+
+let poly1 = new Polynomial();
+
+expect.
+
+// Question #2:
+/* I need you to write some a function that's going to be able to store up to 10 objects which are going to be average 80MB in size each, which I'll later need to access. How would you store them? Provide as detailed an explanation as you can, including any other considerations that would influence your design decision. */
+
+
